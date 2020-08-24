afde941915519155bb545457f3a68253cb509f28
Cody-Blair <cb.rialbc@gmail.com>
2020-08-23 22:00:15 -0600

    doomyguy first animated

---
+++ script.js
@@ -0,0 +1,11 @@
+myProfilePic
+
+function profileVanish () {
+    let profilePic = document.getElementById('myProfilePic')
+    onclick = profilePic.classList.toggle('show')
+}
+
+function phoneAppear () {
+    let phoneVanish = document.getElementById('phoneNumber')
+    onclick = phoneVanish.classList.toggle('show')
+}
